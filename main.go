273d6e1e9bb9cfeae8a6fd50c7fff7491859d842
fujit4 <fujitash4@gmail.com>
2020-07-01 12:31:27 +0000

    add start log

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -10,6 +11,7 @@ func main() {
 
 	var port = flag.String("port", "8282", "port number")
 	var rootdir = flag.String("root", ".", "root directory of fileserver")
+	fmt.Println("start" + " " + "port:" + *port + " " + "root dir:" + *rootdir)
 	// Simple static webserver:
 	http.Handle("/", http.FileServer(http.Dir(*rootdir)))
 	log.Fatal(http.ListenAndServe(":"+*port, nil))
